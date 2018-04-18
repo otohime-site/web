5eb8809fe4fe19df3529a3ee705cf684c8378be9
Hsiao-Ting Yu <sst.dreams@gmail.com>
2018-04-18 21:56:54 +0800

    Add Book.jsx.

---
+++ src/Book.jsx
@@ -0,0 +1,14 @@
+import { Component } from 'react';
+import './Book.css';
+
+export default class Book extends Component {
+  state: {
+  }
+  render() {
+    return (
+      <div className="Book">
+        <button>Update</button>
+      </div>
+    );
+  }
+}
