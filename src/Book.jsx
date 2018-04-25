7f9a49c7e9f4266cd09c3ee8a3ccdf733bcffbe3
Hsiao-Ting Yu <sst.dreams@gmail.com>
2018-04-26 02:08:07 +0800

    Implement nickname finding.

--- src/Book.jsx
+++ src/Book.jsx
@@ -6,11 +6,23 @@ export default class Book extends Component {
   state = {
     progress: 0,
     nickname: '',
+    myNicknames: [],
+  };
+  componentDidMount = async () => {
+    const res = await fetch('https://localhost:5000/api/mai/me', {
+      credentials: 'include',
+    });
+    const results = await res.json();
+    const myNicknames = [];
+    for (let i = 0; i < results.length; i += 1) {
+      myNicknames.push(results[i].nickname);
+    }
+    this.setState({ myNicknames });
   };
   handleUpdate = async () => {
     try {
       const result = await parser(this.handleProgress); // eslint-disable-line no-unused-vars
-      result.icon = '1524338941329';
+      result.icon = 'na';
       await fetch(`https://localhost:5000/api/mai/${this.state.nickname}`, {
         method: 'POST',
         body: JSON.stringify(result),
@@ -23,16 +35,26 @@ export default class Book extends Component {
     }
   };
 
+  handleRadioChange = (e) => {
+    this.setState({ nickname: e.target.value });
+  };
+
   handleProgress = async (progress) => {
     this.setState({ progress });
     return new Promise(resolve => setTimeout(resolve, 1000));
   };
 
   render() {
+    const radios = [];
+    for (let i = 0; i < this.state.myNicknames.length; i += 1) {
+      const nickname = this.state.myNicknames[i];
+      radios.push(<p key={nickname}><input type="radio" name="nickname" value={nickname} onChange={this.handleRadioChange} /> {nickname}</p>);
+    }
     return (
       <div className="Book">
         <h3>Updater</h3>
-        <p>Nickname: <input type="text" value={this.state.nickname} onChange={(e) => { this.setState({ nickname: e.target.value }); }} /></p>
+        <p>Nickname:</p>
+        { radios }
         <p>
           <button onClick={this.handleUpdate}>Update</button>
           <progress value={this.state.progress} max="100" />
