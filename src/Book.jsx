8ff2d17998f100c6dbcc16bbc60a4cbd5a66d105
Hsiao-Ting Yu <sst.dreams@gmail.com>
2018-04-25 23:24:49 +0800

    Add score sending.

--- src/Book.jsx
+++ src/Book.jsx
@@ -5,11 +5,19 @@ import './Book.css';
 export default class Book extends Component {
   state = {
     progress: 0,
+    nickname: '',
   };
-
   handleUpdate = async () => {
     try {
       const result = await parser(this.handleProgress); // eslint-disable-line no-unused-vars
+      result.icon = '1524338941329';
+      await fetch(`https://localhost:5000/api/mai/${this.state.nickname}`, {
+        method: 'POST',
+        body: JSON.stringify(result),
+        headers: { 'Content-Type': 'application/json' },
+        credentials: 'include',
+      });
+      alert('OK!'); // eslint-disable-line no-alert
     } catch (e) {
       alert('Error!'); // eslint-disable-line no-alert
     }
@@ -24,6 +32,7 @@ export default class Book extends Component {
     return (
       <div className="Book">
         <h3>Updater</h3>
+        <p>Nickname: <input type="text" value={this.state.nickname} onChange={(e) => { this.setState({ nickname: e.target.value }); }} /></p>
         <p>
           <button onClick={this.handleUpdate}>Update</button>
           <progress value={this.state.progress} max="100" />
