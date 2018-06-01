c958ce29b5dcc3fc359a093a5b8b58704e1b10bd
Hsiao-Ting Yu <sst.dreams@gmail.com>
2018-06-02 01:45:52 +0800

    Fix the bookmarklet launcher.

--- build-bookmarklet.js
+++ build-bookmarklet.js
@@ -8,16 +8,16 @@ async function build() {
   const runtimeUrl = $('script').eq(0).attr('src');
   const jsUrl = $('script').eq(1).attr('src');
   const goJsContent = `
-var l = document.createElment('link');
+var l = document.createElement('link');
 l.rel = 'stylesheet';
 l.href = 'https://smq.moe${cssUrl}';
-document.appendChild(l);
+document.body.appendChild(l);
 var s1 = document.createElement('script');
 s1.src = 'https://smq.moe${runtimeUrl}';
-document.appendChild(s1);
+document.body.appendChild(s1);
 var s2 = document.createElement('script');
 s2.src = 'https://smq.moe${jsUrl}';
-document.appendChild(s2);
+document.body.appendChild(s2);
   `;
   await fsPromises.writeFile('./build/go.js', goJsContent);
   await fsPromises.unlink('./build/go.html');
