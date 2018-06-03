1d0bb5d34d2f27ca41f7596d1b6c22a50526ba15
Hsiao-Ting Yu <sst.dreams@gmail.com>
2018-06-04 00:23:59 +0800

    Add a var to bookmarklet.

--- src/Home.jsx
+++ src/Home.jsx
@@ -5,7 +5,7 @@ export default function home() {
     <div>
       封測中！
       {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-      <p><a href={`javascript:s=document.createElement("script");s.src="https://${host}/go.js";document.body.appendChild(s);void(0);`} className="btn">Bookmarklet</a></p>
+      <p><a href={`javascript:var s=document.createElement("script");s.src="https://${host}/go.js";document.body.appendChild(s);void(0);`} className="btn">Bookmarklet</a></p>
     </div>
   );
 }
