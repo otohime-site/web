799dbe4ad7c2929f021b8e4f577d72ffa0eab4b9
Hsiao-Ting Yu <sst.dreams@gmail.com>
2019-02-06 22:09:29 +0800

    Rewrite player component, add level grouping.

--- src/laundry/components/Player.tsx
+++ src/laundry/components/Player.tsx
@@ -2,15 +2,17 @@ import React, { FunctionComponent, useCallback, useEffect } from 'react'
 import { useMappedState, useDispatch } from 'redux-react-hook'
 import { Link } from 'react-router-dom'
 import useRouter from 'use-react-router'
-import { Label, Checkbox, Loader, Table, Button, Dropdown, CheckboxProps, DropdownProps } from 'semantic-ui-react'
-import Score from './Score'
-import Record from './Record'
+import { Accordion, Label, Checkbox, Loader, Table, Button,
+         Dropdown, CheckboxProps, DropdownProps } from 'semantic-ui-react'
+import ScoreComponent from './Score'
+import RecordComponent from './Record'
 import {
   getPlayer, getSongs, setShowDifficulties, setSort, getMe
 } from '../actions'
 import './Player.css'
 import { RootState } from '../../reducers'
-import { Sort } from '../types'
+import { Sort, Song, Score } from '../types'
+import { difficulties } from '../consts'
 
 const sortDropdownOptions = [
   {
@@ -22,6 +24,11 @@ const sortDropdownOptions = [
     key: 'version',
     text: '版本',
     value: 'version'
+  },
+  {
+    key: 'level',
+    text: '樂曲等級',
+    value: 'level'
   }
 ]
 const versions = new Map([
@@ -39,6 +46,95 @@ const versions = new Map([
   [1.5, 'PLUS'],
   [1, 'maimai']
 ])
+
+const levelSorter = (a: string, b: string) => (
+  parseFloat(a.replace(/\+/, '.5')) - parseFloat(b.replace(/\+/, '.5'))
+)
+
+const sameLevelSongSorter = (a: SongWithDifficulty, b: SongWithDifficulty) => {
+  const difficultyDiff = a.difficulty! - b.difficulty!
+  if (difficultyDiff === 0) {
+    return a.seq - b.seq
+  } else {
+    return difficultyDiff
+  }
+}
+
+interface SongWithDifficulty extends Song {
+  difficulty?: number
+}
+
+const tableHeader = (sort: Sort) => {
+  if (sort === 'level') {
+    return (
+      <thead>
+        <tr>
+          <th />
+          <th className='score'>Score</th>
+        </tr>
+      </thead>
+    )
+  } else {
+    const diffNodes = difficulties.map((d, i) => {
+      return (
+        <th className={`score difficulty-${i}`} key={`difficulty-${i}`}>
+          {d}
+        </th>)
+    })
+    return (
+      <thead>
+        <tr>
+          <th />
+          {diffNodes}
+        </tr>
+      </thead>
+    )
+  }
+}
+
+const scoreCell = (score: Score, label: string, i: number) => {
+  if (score) {
+    let className = `score difficulty-${i}`
+    if (score.score === 0) {
+      className += ' score-zero'
+    }
+    return (
+      <td
+        className={className}
+        key={`difficulty-${i}`}
+      >
+        <span className='level'>{label}</span>
+        <ScoreComponent score={score} />
+      </td>
+    )
+
+  } else {
+    return (<td key={`difficulty-${i}`} />)
+  }
+}
+
+const tableRow = (song: SongWithDifficulty, scores: Score[] = [], sort: Sort) => {
+  let scoreCols
+  if (sort === 'level') {
+    const difficulty = song.difficulty!
+    scoreCols = [scoreCell(scores[difficulty], difficulties[difficulty], difficulty)]
+  } else {
+    scoreCols = scores.map((score, i) => (
+      scoreCell(
+        score, (song.levels) ? song.levels[i] || '' : '', i
+      )
+    ))
+  }
+  return (
+    <tr key={song.id}>
+      <td className='song-name'>
+        {song.name}
+        {(song.english_name) ? <p className='english-name'>{song.english_name}</p> : ''}
+      </td>
+      {scoreCols}
+    </tr>
+  )
+}
 
 const PlayerComponent: FunctionComponent = () => {
   const { match } = useRouter<{ nickname: string }>()
@@ -67,84 +163,6 @@ const PlayerComponent: FunctionComponent = () => {
   const changeSort = (e: React.SyntheticEvent, { value }: DropdownProps) => (
     dispatch(setSort(value as Sort))
   )
-  if (!songs || !scores || !record) {
-    return <Loader active={true} />
-  }
-  const tbodies = []
-  let songGroups = new Map()
-  // Assume sortBy to be 'category' for unexpected values.
-  for (let i = 0; i < songs.length; i += 1) {
-    const song = songs[i]
-    const { active, category, version } = song
-    let groupKey = category
-    if (sort === 'version') {
-      groupKey = version
-    }
-    if (!songGroups.has(groupKey)) {
-      songGroups.set(groupKey, [])
-    }
-    if (active) {
-      songGroups.get(groupKey).push(song)
-    }
-  }
-  if (sort === 'version') {
-    songGroups = new Map([...songGroups.entries()].sort())
-  }
-  // Render song groups as <tbody>s.
-  songGroups.forEach((songsInGroup, groupKey) => {
-    const rows = []
-    rows.push((// eslint-disable-next-line react/no-array-index-key
-      <tr key={groupKey}>
-        <th><Label size='large' ribbon={true}>{(sort === 'version') ? versions.get(groupKey) : groupKey}</Label></th>
-        <th className='score difficulty-0'>Easy</th>
-        <th className='score difficulty-1'>Basic</th>
-        <th className='score difficulty-2'>Advanced</th>
-        <th className='score difficulty-3'>Expert</th>
-        <th className='score difficulty-4'>Master</th>
-        <th className='score difficulty-5'>Re:Master</th>
-      </tr>
-    ))
-    songsInGroup.forEach((song) => {
-      const scoresOutput = []
-      if (scores && scores[song.id]) {
-        const scoresOfSong = scores[song.id]
-        for (let j = 0; j < scoresOfSong.length; j += 1) {
-          if (scoresOfSong[j]) {
-            const score = scoresOfSong[j]
-            let className = `score difficulty-${j}`
-            if (score.score === 0) {
-              className += ' score-zero'
-            }
-            scoresOutput.push((
-              <td
-                className={className}
-                key={`difficulty-${j}`}
-              >
-                <span className='level'>{(song.levels) ? song.levels[j] : ''}</span>
-                <Score score={score} />
-              </td>
-            ))
-          } else {
-            scoresOutput.push((<td key={`difficulty-${j}`} />))
-          }
-        }
-      }
-      rows.push((
-        <tr key={song.id}>
-          <td className='song-name'>
-            {song.name}
-            {(song.english_name) ? <p className='english-name'>{song.english_name}</p> : ''}
-          </td>
-          {scoresOutput}
-        </tr>
-      ))
-    })
-    tbodies.push((
-      <tbody>
-        {rows}
-      </tbody>
-    ))
-  })
   if (getPlayerResult) {
     if (getPlayerResult.status === 'err') {
       if (getPlayerResult.err.code === 404) {
@@ -157,20 +175,90 @@ const PlayerComponent: FunctionComponent = () => {
   } else {
     return <Loader active={true} />
   }
+  if (!songs || !scores || !record) {
+    return <Loader active={true} />
+  }
+  const tbodies: JSX.Element[] = []
+  // Arrange song groups based on what grouping we want.
+  let songGroups = songs.reduce((group, song) => {
+    const { active, category, version, levels } = song
+    if (!active) {
+      return group
+    }
+    let groupKeys: string[] = []
+    switch (sort) {
+      case 'category':
+        groupKeys = [ category ]
+        break
+      case 'version':
+        groupKeys = [ `${version}` ]
+        break
+      case 'level':
+        groupKeys = levels.filter(l => l).map(l => `${l}`)
+    }
+    groupKeys.forEach((groupKey, index) => {
+      const songWithDifficulty = {
+        ...song,
+        ...((sort === 'level') ? { difficulty: index } : {})
+      }
+      if (!group.has(groupKey)) {
+        group.set(groupKey, [songWithDifficulty])
+      } else {
+        group.get(groupKey)!.push(songWithDifficulty)
+      }
+    })
+    return group
+  }, new Map<string, SongWithDifficulty[]>()
+  )
+
+  // Sort the key if we are under level or version grouping.
+  const songGroupKeys = [...songGroups.keys()]
+  if (sort === 'level') {
+    songGroupKeys.sort(levelSorter)
+  } else if (sort === 'version') {
+    songGroupKeys.sort()
+  }
+
+  // Render song groups with scores.
+  const result = songGroupKeys.map((groupKey) => {
+    const songInGroup = songGroups.get(groupKey)!
+    if (sort === 'level') {
+      songInGroup.sort(sameLevelSongSorter)
+    }
+    const songRows = songInGroup.map(song => (
+      tableRow(song, scores[song.id], sort)
+    ))
+    return ({
+      key: groupKey,
+      title: { content: (
+        <Label size='large' ribbon={true}>
+          <i className='dropdown icon' />
+          {(sort === 'level') ? 'LEVEL ' : ''}
+          {(sort === 'version') ? versions.get(parseFloat(groupKey)) : groupKey}
+          {' '}({songRows.length})
+        </Label>
+      )},
+      content: { content: (
+        <Table className={(showDifficulties || sort === 'level') ? `player-scores sort-${sort}` : `player-scores sort-${sort} hide-difficulties`} lang='ja'>
+          {tableHeader(sort)}
+          <Table.Body>
+            {songRows}
+          </Table.Body>
+        </Table>
+      )}
+    })
+  })
   return (
     <div>
-      <Record record={record} />
+      <RecordComponent record={record} />
       <div className='player-options'>
         <Button as={Link} to={`/mai/${match.params.nickname}/timeline`}>歷史紀錄</Button>
         排序：
           <Dropdown selection={true} options={sortDropdownOptions} defaultValue='category' onChange={changeSort} />
-        <Checkbox toggle={true} label='顯示所有難易度' onChange={changeShowDifficulties} />
+        {(sort !== 'level') ? <Checkbox toggle={true} label='顯示所有難易度' onChange={changeShowDifficulties} /> : <></>}
       </div>
-      <Table className={(showDifficulties) ? 'player-scores' : 'player-scores hide-difficulties'} lang='ja'>
-        {tbodies}
-      </Table>
+      <Accordion panels={result} exclusive={false} fluid={true} />
     </div>
   )
 }
-
 export default PlayerComponent
--- src/laundry/types.ts
+++ src/laundry/types.ts
@@ -29,7 +29,7 @@ export interface Song {
   active: boolean
   japan_only: boolean
   version: number
-  levels: string[]
+  levels: (string | null)[]
 }
 
 export interface Record {
