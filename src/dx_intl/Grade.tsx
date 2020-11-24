fa290faa4208e65032ef6f83510540aaf4254986
Hsiao-Ting Yu <sst.dreams@gmail.com>
2020-11-25 00:06:10 +0800

    feat: add grade SVG works

--- src/dx_intl/Grade.tsx
+++ src/dx_intl/Grade.tsx
@@ -1,5 +1,17 @@
 import React, { FunctionComponent } from 'react'
 import styled from '../styled'
+import Grade1 from './grades/1.svg'
+import Grade2 from './grades/2.svg'
+import Grade3 from './grades/3.svg'
+import Grade4 from './grades/4.svg'
+import Grade5 from './grades/5.svg'
+import Grade6 from './grades/6.svg'
+import Grade7 from './grades/7.svg'
+import Grade8 from './grades/8.svg'
+import Grade9 from './grades/9.svg'
+import Grade10 from './grades/10.svg'
+import Grade11 from './grades/11.svg'
+import Grade12 from './grades/12.svg'
 import Grade13 from './grades/13.svg'
 import Grade14 from './grades/14.svg'
 
@@ -19,6 +31,30 @@ const GradeContainer = styled('img')`
 
 const getGradeImage = (grade: number): any => {
   switch (grade) {
+    case 1:
+      return Grade1
+    case 2:
+      return Grade2
+    case 3:
+      return Grade3
+    case 4:
+      return Grade4
+    case 5:
+      return Grade5
+    case 6:
+      return Grade6
+    case 7:
+      return Grade7
+    case 8:
+      return Grade8
+    case 9:
+      return Grade9
+    case 10:
+      return Grade10
+    case 11:
+      return Grade11
+    case 12:
+      return Grade12
     case 13:
       return Grade13
     case 14:
