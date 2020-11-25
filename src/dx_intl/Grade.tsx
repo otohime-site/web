8b36c4c789d96b9cc0f658fd5552edf998d1332b
Hsiao-Ting Yu <sst.dreams@gmail.com>
2020-11-25 09:48:05 +0800

    feat: finishing grades

--- src/dx_intl/Grade.tsx
+++ src/dx_intl/Grade.tsx
@@ -14,6 +14,17 @@ import Grade11 from './grades/11.svg'
 import Grade12 from './grades/12.svg'
 import Grade13 from './grades/13.svg'
 import Grade14 from './grades/14.svg'
+import Grade15 from './grades/15.svg'
+import Grade16 from './grades/16.svg'
+import Grade17 from './grades/17.svg'
+import Grade18 from './grades/18.svg'
+import Grade19 from './grades/19.svg'
+import Grade20 from './grades/20.svg'
+import Grade21 from './grades/21.svg'
+import Grade22 from './grades/22.svg'
+import Grade23 from './grades/23.svg'
+import Grade24 from './grades/24.svg'
+import Grade25 from './grades/25.svg'
 
 export const gradeNames = [
   '', // start from 1 :(
@@ -31,34 +42,31 @@ const GradeContainer = styled('img')`
 
 const getGradeImage = (grade: number): any => {
   switch (grade) {
-    case 1:
-      return Grade1
-    case 2:
-      return Grade2
-    case 3:
-      return Grade3
-    case 4:
-      return Grade4
-    case 5:
-      return Grade5
-    case 6:
-      return Grade6
-    case 7:
-      return Grade7
-    case 8:
-      return Grade8
-    case 9:
-      return Grade9
-    case 10:
-      return Grade10
-    case 11:
-      return Grade11
-    case 12:
-      return Grade12
-    case 13:
-      return Grade13
-    case 14:
-      return Grade14
+    case 1: return Grade1
+    case 2: return Grade2
+    case 3: return Grade3
+    case 4: return Grade4
+    case 5: return Grade5
+    case 6: return Grade6
+    case 7: return Grade7
+    case 8: return Grade8
+    case 9: return Grade9
+    case 10: return Grade10
+    case 11: return Grade11
+    case 12: return Grade12
+    case 13: return Grade13
+    case 14: return Grade14
+    case 15: return Grade15
+    case 16: return Grade16
+    case 17: return Grade17
+    case 18: return Grade18
+    case 19: return Grade19
+    case 20: return Grade20
+    case 21: return Grade21
+    case 22: return Grade22
+    case 23: return Grade23
+    case 24: return Grade24
+    case 25: return Grade25
   }
   return ''
 }
