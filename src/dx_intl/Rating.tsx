637465aa4f163bc1c672a2170536fe92acd0f5ba
Hsiao-Ting Yu <sst.dreams@gmail.com>
2020-11-25 09:01:28 +0800

    feat: add all rating images

--- src/dx_intl/Rating.tsx
+++ src/dx_intl/Rating.tsx
@@ -1,8 +1,39 @@
 import React, { FunctionComponent } from 'react'
 import Normal from './ratings/normal.svg'
+import Blue from './ratings/blue.svg'
+import Green from './ratings/green.svg'
+import Orange from './ratings/orange.svg'
+import Red from './ratings/red.svg'
+import Purple from './ratings/purple.svg'
+import Bronze from './ratings/bronze.svg'
+import Silver from './ratings/silver.svg'
+import Gold from './ratings/gold.svg'
+import Rainbow from './ratings/rainbow.svg'
 import styled from '../styled'
 
 const getRatingImage = (rating: number): any => {
+  switch (true) {
+    case (rating < 1000):
+      return Normal
+    case (rating < 2000):
+      return Blue
+    case (rating < 3000):
+      return Green
+    case (rating < 4000):
+      return Orange
+    case (rating < 5000):
+      return Red
+    case (rating < 6000):
+      return Purple
+    case (rating < 7000):
+      return Bronze
+    case (rating < 8000):
+      return Silver
+    case (rating < 8500):
+      return Gold
+    case (isFinite(rating)):
+      return Rainbow
+  }
   return Normal
 }
 
