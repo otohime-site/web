00e2f913d28b1fb96b199614a0f83162ba8694b4
Hsiao-Ting Yu <sst.dreams@gmail.com>
2020-11-27 10:51:08 +0800

    feat: add variant components

---
+++ src/dx_intl/Variant.tsx
@@ -0,0 +1,14 @@
+import React, { FunctionComponent } from 'react'
+import STD from './variants/std.svg'
+import DX from './variants/dx.svg'
+import styled from '../styled'
+
+const VariantImage = styled('img')`
+  height: 1em;
+`
+
+const Variant: FunctionComponent<{
+  deluxe: boolean
+}> = ({ deluxe }) => <VariantImage src={(deluxe) ? DX : STD} />
+
+export default Variant
