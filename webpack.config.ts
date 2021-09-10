53717303f268720d4a1adebc4e64883e301a300c
Hsiao-Ting Yu <sst.dreams@gmail.com>
2021-09-10 22:27:25 +0800

    feat: use favicons plugin

--- webpack.config.ts
+++ webpack.config.ts
@@ -3,6 +3,7 @@ import { Configuration } from "webpack"
 import { Configuration as DevServerConfiguration } from "webpack-dev-server"
 import MiniCssExtractPlugin from "mini-css-extract-plugin"
 import HTMLWebpackPlugin from "html-webpack-plugin"
+import FaviconsWebpackPlugin from "favicons-webpack-plugin"
 import { BookmarkletPlugin } from "./bookmarklet-plugin"
 
 // Workaround for @types/webpack-dev-server versions...
@@ -22,10 +23,10 @@ const config: Configuration & { devServer?: DevServerConfiguration } = {
   plugins: [
     new MiniCssExtractPlugin(),
     new HTMLWebpackPlugin({
-      favicon: "src/logo/icon2.svg",
       template: "src/index.html",
       chunks: ["index"],
     }),
+    new FaviconsWebpackPlugin("src/logo/icon2.svg"),
     new BookmarkletPlugin(),
   ],
   module: {
