eeb42e30210141aebfd88d4d3f98ee8d524140d7
Hsiao-Ting <sst.dreams@gmail.com>
2022-09-10 00:32:11 +0800

    revert to Vite 2 as we hit https://github.com/vitejs/vite/issues/8464

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
-import basicSsl from "@vitejs/plugin-basic-ssl"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 export default defineConfig({
-  plugins: [react(), basicSsl()],
+  plugins: [react()],
   build: {
     emptyOutDir: false, // as the library will empty it
   },
