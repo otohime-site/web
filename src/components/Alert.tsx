1dc132eaa37c43f6955f6b8352b44a041609d00c
Hsiao-Ting Yu <sst.dreams@gmail.com>
2022-11-16 00:52:56 +0800

    feat: add more UI

--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -1,7 +1,21 @@
 import { styled } from "./stitches.config"
 
 export const Alert = styled("div", {
+  padding: "8px",
   variants: {
-    severity: { info: {}, error: {}, warning: {} },
+    severity: {
+      info: {
+        background: "$indigo3",
+        color: "$indigo12",
+      },
+      error: {
+        background: "$red3",
+        color: "$red12",
+      },
+      warning: {
+        background: "$yellow3",
+        color: "$yellow12",
+      },
+    },
   },
 })
--- src/components/stitches.config.ts
+++ src/components/stitches.config.ts
@@ -1,7 +1,13 @@
+import { gray, indigo, red, yellow } from "@radix-ui/colors"
 import { createStitches } from "@stitches/react"
 
 export const { styled } = createStitches({
   theme: {
-    colors: {},
+    colors: {
+      ...gray,
+      ...yellow,
+      ...red,
+      ...indigo,
+    },
   },
 })
