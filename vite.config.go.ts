86997e1f7491881d52a8692bd1e2132479ff8820
Hsiao-Ting Yu <sst.dreams@gmail.com>
2022-12-27 01:40:25 +0800

    feat: upgrade to vite 4

--- vite.config.go.ts
+++ vite.config.go.ts
@@ -1,9 +1,10 @@
 import react from "@vitejs/plugin-react"
 import { resolve } from "path"
+import { visualizer } from "rollup-plugin-visualizer"
 import { defineConfig } from "vite"
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), (visualizer as any)()],
   build: {
     lib: {
       entry: resolve(__dirname, "src/go.tsx"),
