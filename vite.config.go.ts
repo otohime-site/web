72da188dc540ded53bdd3d656245bcacf163815d
Hsiao-Ting Yu <sst.dreams@gmail.com>
2023-01-09 00:34:51 +0800

    fix: fix bookmarklet vite config

--- vite.config.go.ts
+++ vite.config.go.ts
@@ -6,11 +6,16 @@ import { defineConfig } from "vite"
 export default defineConfig({
   plugins: [react(), (visualizer as any)()],
   build: {
-    lib: {
-      entry: resolve(__dirname, "src/go.tsx"),
-      formats: ["iife"],
-      name: "go",
-      fileName: () => "go.js",
+    // Using library mode will result in large bundle
+    // https://stackoverflow.com/questions/73713323/
+    rollupOptions: {
+      input: resolve(__dirname, "src/go.tsx"),
+      output: {
+        format: "iife",
+        dir: resolve(__dirname, "dist"),
+        entryFileNames: "go.js",
+        assetFileNames: "go.css",
+      },
     },
   },
 })
