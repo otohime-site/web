368841fb44dd5c399f0e3f95b763c6c81936fd0f
Hsiao-Ting <sst.dreams@gmail.com>
2023-04-03 00:08:54 +0800

    feat: fix variant

--- src/dx_intl/components/Variant.tsx
+++ src/dx_intl/components/Variant.tsx
@@ -3,7 +3,7 @@ import STD from "../images/variants/std.svg"
 import classes from "./Variant.module.css"
 
 const Variant = ({ deluxe }: { deluxe: boolean }) => (
-  <img className={classes.img} src={deluxe ? DX : STD} />
+  <img className={classes.variant} src={deluxe ? DX : STD} />
 )
 
 export default Variant
