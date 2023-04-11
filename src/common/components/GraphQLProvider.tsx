e64d84445b9ae60240b4a578057be12d5702ebdb
Hsiao-Ting Yu <sst.dreams@gmail.com>
2023-04-12 00:33:52 +0800

    feat: update urql, remove subscription

--- src/common/components/GraphQLProvider.tsx
+++ src/common/components/GraphQLProvider.tsx
@@ -1,9 +1,4 @@
-import {
-  cacheExchange,
-  dedupExchange,
-  fetchExchange,
-  makeOperation,
-} from "@urql/core"
+import { cacheExchange, fetchExchange } from "@urql/core"
 import { authExchange } from "@urql/exchange-auth"
 import { PropsWithChildren, useMemo } from "react"
 import { createClient, Provider as UrqlProvider } from "urql"
@@ -12,45 +7,32 @@ import { apiHost } from "../../host"
 
 const GraphQLProvider = ({ children }: PropsWithChildren<unknown>) => {
   const [user] = useAuth()
-
   const client = useMemo(
     () =>
       createClient({
         url: `https://${apiHost}/graphql`,
         exchanges: [
-          dedupExchange,
           cacheExchange,
-          authExchange({
-            getAuth: async () => {
-              if (user == null) {
-                return null
-              }
-              const token = await user.getIdToken()
-              return { token }
-            },
-            addAuthToOperation: ({ authState, operation }) => {
-              if (
-                authState == null ||
-                authState.token == null ||
-                operation.kind === "subscription"
-              ) {
-                return operation
-              }
-              const fetchOptions =
-                typeof operation.context.fetchOptions === "function"
-                  ? operation.context.fetchOptions()
-                  : operation.context.fetchOptions ?? {}
-              return makeOperation(operation.kind, operation, {
-                ...operation.context,
-                fetchOptions: {
-                  ...fetchOptions,
-                  headers: {
-                    ...fetchOptions.headers,
-                    Authorization: `Bearer ${authState.token}`,
-                  },
-                },
-              })
-            },
+          authExchange(async (utils) => {
+            const token = await user?.getIdToken()
+            return {
+              addAuthToOperation(operation) {
+                return token
+                  ? utils.appendHeaders(operation, {
+                      Authorization: `Bearer ${token}`,
+                    })
+                  : operation
+              },
+              didAuthError(error) {
+                return error.graphQLErrors.some(
+                  (e) => e.extensions?.code === "access-denied"
+                )
+              },
+              async refreshAuth() {
+                // getIdToken should do the job
+                return
+              },
+            }
           }),
           fetchExchange,
         ],
