457e428685c2b3ea1d933686d77c618ce0af84fb
Hsiao-Ting <sst.dreams@gmail.com>
2023-05-29 00:47:01 +0800

    feat: add score cell wip

---
+++ src/dx_intl/components/ScoreCell.tsx
@@ -0,0 +1,21 @@
+import { comboFlags, levels, syncFlags } from "../models/constants"
+import { ComboFlag, SyncFlag } from "./Flags"
+
+export const ScoreCell = ({
+  data: { difficulty, level, score, combo_flag, sync_flag },
+}: {
+  data: {
+    difficulty: number
+    level: (typeof levels)[number]
+    score: number
+    combo_flag: (typeof comboFlags)[number]
+    sync_flag: (typeof syncFlags)[number]
+  }
+}) => {
+  ;<>
+    {difficulty}
+    {level}
+    {score}
+    <ComboFlag flag={combo_flag} /> <SyncFlag flag={sync_flag} />
+  </>
+}
