578d1069d72d21fcdad2d2f883df398cb334d55e
Hsiao-Ting Yu <sst.dreams@gmail.com>
2023-06-01 02:10:53 +0800

    feat: add a group by util

---
+++ src/common/utils/grouping.ts
@@ -0,0 +1,13 @@
+// Watch https://github.com/tc39/proposal-array-grouping later
+
+export const groupByKey = <T, K extends keyof T>(
+  arr: T[],
+  key: K
+): Map<T[K], T[]> =>
+  arr.reduce((prev, curr) => {
+    const k = curr[key]
+    const v = prev.get(k) ?? []
+    v.push(curr)
+    prev.set(k, v)
+    return prev
+  }, new Map<T[K], T[]>())
