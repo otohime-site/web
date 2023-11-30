db007284b0fc3ae5e22c1634da4ac28e6c0973a9
Hsiao-Ting Yu <sst.dreams@gmail.com>
2023-11-30 23:56:44 +0800

    feat: add postcss-nesting

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
 import OpenProps from "open-props"
 import postcssJitProps from "postcss-jit-props"
+import postcssNesting from "postcss-nesting"
 
 export default {
-  plugins: [postcssJitProps(OpenProps)],
+  plugins: [postcssJitProps(OpenProps), postcssNesting()],
 }
