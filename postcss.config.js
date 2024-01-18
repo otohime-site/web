f3650f99ec549bdf2a4669d76feca6cd92795a9a
Hsiao-Ting Yu <sst.dreams@gmail.com>
2024-01-19 01:14:45 +0800

    feat: add postcss-preset-env

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,8 @@
 import OpenProps from "open-props"
 import postcssJitProps from "postcss-jit-props"
 import postcssNesting from "postcss-nesting"
+import postcssPresetEnv from "postcss-preset-env"
 
 export default {
-  plugins: [postcssJitProps(OpenProps), postcssNesting()],
+  plugins: [postcssJitProps(OpenProps), postcssNesting(), postcssPresetEnv()],
 }
