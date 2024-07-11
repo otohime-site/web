52a58298bcd6309984cb812fbc315a0c9ef249ce
Hsiao-Ting Yu <sst.dreams@gmail.com>
2024-07-12 04:41:16 +0800

    feat: add finale flag wip

--- src/finale/components/Flags.tsx
+++ src/finale/components/Flags.tsx
@@ -1,9 +1,30 @@
+import clsx from "clsx"
 import { comboFlags, syncFlags } from "../models/constants"
+import classes from "./Flag.module.css"
 
-export const ComboFlag = ({ flag }: { flag: (typeof comboFlags)[number] }) => (
-  <>{flag} </>
-)
+const comboFlagNames = {
+  fc_silver: "FC",
+  fc_gold: "FC",
+  ap: "AP",
+  ap_plus: "AP+",
+} as const
+
+export const ComboFlag = ({ flag }: { flag: (typeof comboFlags)[number] }) =>
+  flag ? (
+    <span
+      className={clsx(
+        classes.flag,
+        flag === "fc_gold" && classes["flag-fc-gold"],
+        flag === "ap" && classes["flag-ap"],
+        flag === "ap_plus" && classes["flag-ap-plus"],
+      )}
+    >
+      {comboFlagNames[flag]}
+    </span>
+  ) : (
+    <></>
+  )
 
 export const SyncFlag = ({ flag }: { flag: (typeof syncFlags)[number] }) => (
-  <>{flag}</>
+  <span className={classes.flag}>{flag}</span>
 )
