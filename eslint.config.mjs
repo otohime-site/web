dca8c36112d68ada397d7d322884f1b6794b24ac
Hsiao-Ting Yu <sst.dreams@gmail.com>
2025-03-04 01:01:48 +0800

    feat: deps update including eslint-plugin-react-hooks

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,18 +8,8 @@ import tseslint from "typescript-eslint"
 
 export default tseslint.config(
   eslint.configs.recommended,
-  // @ts-expect-error https://github.com/jsx-eslint/eslint-plugin-react/issues/3838
   reactPlugin.configs.flat.recommended,
   reactPlugin.configs.flat["jsx-runtime"],
-  ...tseslint.configs.recommended,
-  // https://github.com/facebook/react/issues/28313#issuecomment-2363692765
-  {
-    plugins: {
-      "react-hooks": reactHooks,
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-    },
-  },
+  reactHooks.configs["recommended-latest"],
   prettier,
 )
