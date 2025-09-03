1a31b69872f14ba4a2e82cb5bafaa2fef0a9555f
Hsiao-Ting Yu <sst.dreams@gmail.com>
2025-09-04 01:01:56 +0800

    feat: update eslint.config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,10 +4,12 @@ import eslint from "@eslint/js"
 import prettier from "eslint-config-prettier/flat"
 import reactPlugin from "eslint-plugin-react"
 import reactHooks from "eslint-plugin-react-hooks"
+import { defineConfig } from "eslint/config"
 import tseslint from "typescript-eslint"
 
-export default tseslint.config(
+export default defineConfig(
   eslint.configs.recommended,
+  tseslint.configs.recommended,
   reactPlugin.configs.flat.recommended,
   reactPlugin.configs.flat["jsx-runtime"],
   reactHooks.configs["recommended-latest"],
