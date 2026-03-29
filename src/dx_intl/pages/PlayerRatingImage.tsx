f67fc39756a37036aebd67fb626e5a5e8e49a762
Hsiao-Ting Yu <sst.dreams@gmail.com>
2026-03-29 12:07:10 +0800

    feat: add font loading

--- src/dx_intl/pages/PlayerRatingImage.tsx
+++ src/dx_intl/pages/PlayerRatingImage.tsx
@@ -1,6 +1,21 @@
+import { useEffect, useState } from "react"
 import { Layer, Stage, Text } from "react-konva"
 
 const RatingImage = () => {
+  const [fontsLoaded, setFontsLoaded] = useState(false)
+
+  useEffect(() => {
+    document.fonts
+      .load('800 30px "M PLUS Rounded 1c"', "空回りライブラリ")
+      .then(() => {
+        setFontsLoaded(true)
+      })
+  }, [])
+
+  if (!fontsLoaded) {
+    return null
+  }
+
   return (
     <Stage width={1000} height={1000}>
       <Layer>
